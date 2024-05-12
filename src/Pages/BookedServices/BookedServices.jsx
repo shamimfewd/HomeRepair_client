aa4ce579363a52352b13ded8ad0e8834c46f5632
shamim <shamimmahmud077@gmail.com>
2024-05-13 00:14:49 +0600

    implement booked services page

--- src/Pages/BookedServices/BookedServices.jsx
+++ src/Pages/BookedServices/BookedServices.jsx
@@ -1,13 +1,44 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
 
 const BookedServices = () => {
-    
-    return (
-        <div>
-            <h1>booked services</h1>
-        </div>
-    );
+  const [bookedServ, setBookedServ] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/bookingData")
+      .then((res) => res.json())
+      .then((data) => {
+        setBookedServ(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookedServ.map((serv) => (
+              <div key={serv._id}>
+                <tr>
+                  <th>1</th>
+                  <td>{serv.serviceName}</td>
+                  <td>Quality Control Specialist</td>
+                  <td>{serv.status}</td>
+                </tr>
+              </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default BookedServices;
